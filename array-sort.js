b3389b2c7e6efcf991c4077aca073dac5c2a4742
rogerfaustino <roger_faustino@hotmail.com>
2020-03-27 09:36:39 -0300

    Adding new features

---
+++ array-sort.js
@@ -0,0 +1,7 @@
+const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food);
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){ return a - b });
+console.log(points);
