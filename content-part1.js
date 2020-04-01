49b81417495306c5906dc6368fbc4ec1410b390d
rogerfaustino <roger_faustino@hotmail.com>
2020-04-01 15:23:10 -0300

    Implementing changes

--- content-part1.js
+++ content-part1.js
@@ -3,7 +3,6 @@ const customer1 = {
   age: 22,
   job: 'Teacher',
 };
-
 console.log(customer1);
 
 customer1.lastName = 'Faria';
@@ -14,12 +13,12 @@ const customer2 = {
   age: 23,
   job: 'Medic',
 };
-
 console.log(customer2);
+
 customer2['lastName'] = 'Silva';
 console.log(customer2);
 
-// ________________________________________________
+console.log('----------------------------------------')
 
 const customer = {
   firstName: 'Roberto',
