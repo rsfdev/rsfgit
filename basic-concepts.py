5af8ac223071957363729f7278ed6b00c8123a81
rogerfaustino <roger_faustino@hotmail.com>
2020-04-08 17:37:21 -0300

    Starting python learning

---
+++ basic-concepts.py
@@ -0,0 +1,18 @@
+print('Hello World!\nSecond Print\tUsing Tab')
+name = 'Keith'
+print(name)
+print(type(name))
+age = 30
+print(type(age))
+value = 13.98
+print(type(value))
+print(name + ' is ' + str(age) + ' years old ')
+phrase = name + ' is ' + str(age) + ' years old '
+print(phrase)
+# name = input('Please write your first name:')
+# print(name)
+
+a = 2
+b = 5
+sum = a + b
+print(sum)
