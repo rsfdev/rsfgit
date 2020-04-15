fb4272605f5c9c34ad076c374fd13285439d5cbe
rogerfaustino <roger_faustino@hotmail.com>
2020-04-15 17:38:49 -0300

    jest mock spyon

--- service.test.js
+++ service.test.js
@@ -1,7 +1,20 @@
+// Matcher error: received value must be a mock or spy function
+// const service = require('./service');
+
+// it("#randomRgbColor", () => {
+//   // testing whether the funciotn was called
+//   service.randomRgbColor();
+//   expect(service.randomRgbColor).toHaveBeenCalled();
+// });
+
 const service = require('./service');
 
 test("#randomRgbColor", () => {
-  // testando se a função foi chamada
+  // testing whether the function was called. We don't simulate any behavior here because, for this test, it doesn't matter! We want to know if she was called, period.
+  service.randomRgbColor = jest.fn().mockReturnValue("rgb(255, 255, 255)");
+
   service.randomRgbColor();
   expect(service.randomRgbColor).toHaveBeenCalled();
+  expect(service.randomRgbColor()).toBe("rgb(255, 255, 255)");
 });
+
