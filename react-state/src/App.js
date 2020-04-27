49cdb839e0cc60d0feae4e62e235c84f152e386c
rogerfaustino <roger_faustino@hotmail.com>
2020-04-27 19:11:15 -0300

    Project updating

--- react-state/src/App.js
+++ react-state/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import colors from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    console.log('sendo construído');
+    super(props);
+    this.state = {
+      filter: '',
+    }
+  }
+
+  setFilterValue = (event) => {
+    console.log('Novo conteúdo do meu input:', event.target.value);
+    this.setState({ filter: event.target.value });
+  }
+
+  filterColors = () => {
+    if(this.state.filter === '') return colors;
+    return colors.filter((element) => element.color === this.state.filter)
+  }
+
+  render() {
+    console.log('estou sendo renderizado!!!');
+    const filteredColors = this.filterColors();
+
+    return (
+      <div className="App">
+        <h1>Filter colors</h1>
+        <div>
+          <input type="text" onChange={this.setFilterValue}/>
+        </div>
+        <ul>
+          {filteredColors.map(({ color, value }) => <li key={value}> {color} coded as {value}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
