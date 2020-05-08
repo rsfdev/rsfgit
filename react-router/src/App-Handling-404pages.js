e83149bb71203d755e417529ea588deb7e3377fa
rogerfaustino <roger_faustino@hotmail.com>
2020-05-08 20:21:46 -0300

    Handling 404 pages

---
+++ react-router/src/App-Handling-404pages.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+
+const Home = () => (
+  <h1>
+    Home
+  </h1>
+)
+
+const WillMatch = () => 
+  <h3>
+    Matched!
+  </h3>
+console.log(WillMatch);
+
+
+const NoMatch = ({ location }) => (
+  <div>
+    <h3>
+      No match for <code>{location.pathname}</code>
+    </h3>
+  </div>
+)
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/will-match">Will Match</Link></li>
+          <li><Link to="/will-not-match">Will Not Match</Link></li>
+          <li><Link to="/also/will/not/match">Also Will Not Match</Link></li>
+        </ul>
+
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/will-match" component={WillMatch}/>
+          <Route component={NoMatch}/>
+        </Switch>
+      </div>
+    </Router>
+    )
+  }
+}
