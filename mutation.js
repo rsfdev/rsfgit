0b531cb166f656c28e51b7e0c6c381f1f5f0e187
rsfdev <roger_faustino@hotmail.com>
2020-05-27 15:23:56 -0300

    Mutation

---
+++ mutation.js
@@ -0,0 +1,5 @@
+const itemPrimario = [1, 2, 3, 4, 5];
+let mutacaoTeste = itemPrimario;
+mutacaoTeste.push(6);
+console.log(mutacaoTeste); // [1, 2, 3, 4, 5, 6]
+console.log(itemPrimario); // [1, 2, 3, 4, 5, 6]
